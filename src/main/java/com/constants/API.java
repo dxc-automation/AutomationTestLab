ba58092f569bbfce664c709d6e9e4a855f9fd869
Pavel Popov <pavelpopov44@gmail.com>
2019-07-25 13:21:11 +0300

    Update API.java

--- src/main/java/com/constants/API.java
+++ src/main/java/com/constants/API.java
@@ -6,8 +6,11 @@ public interface API {
 
   //****    P A T H S     ****//
 
-  String external_login = "/sportsbook/v1/api/externalLogin";
-  String get_open_bets = "/sportsbook/v1/api/getOpenBets";
-  String place_bets = "/sportsbook/v1/api/placeBets";
-  String get_bet_status = "/PsAmelcoApi/getBetStatus";
+  String external_login     = "/sportsbook/v1/api/externalLogin";
+  String get_open_bets      = "/sportsbook/v1/api/getOpenBets";
+  String place_bets         = "/sportsbook/v1/api/placeBets";
+  String get_bet_status     = "/PsAmelcoApi/getBetStatus";
+  String calculate_cashout  = "/sportsbook/v1/api/placeBets";
+  String cashout            = "/sportsbook/v1/api/cashoutBet";
+  String get_sports_tree    = "/sportsbook/v1/api/getSportTree";
 }
