25278a27aacad68f2a188badda223aab3e83ea3d
starts_qa_team <48771359+dxc-automation@users.noreply.github.com>
2020-03-20 21:00:05 +0200

    fix

--- src/main/java/com/demo/objects/LoginPage.java
+++ src/main/java/com/demo/objects/LoginPage.java
@@ -14,13 +14,13 @@ public class LoginPage extends BasicTestConfig {
 
     final WebDriver driver;
 
-    @FindBy(how = How.CSS, using = "input#username.nmcEvmpQGs-DUsPuI4fzO")
+    @FindBy(how = How.CSS, using = "input#username")
     public WebElement login_user_input;
 
-    @FindBy(how = How.CSS, using = "input#password.nmcEvmpQGs-DUsPuI4fzO.swuIwUNQwFAucmifmmWO4")
+    @FindBy(how = How.CSS, using = "input#password")
     public WebElement login_pass_input;
 
-    @FindBy(how = How.CSS, using = "button._6iSTUGFRm1Pp_z-8_y0Y5.Zte1YssVqY438XSyivt7f")
+    @FindBy(how = How.CSS, using = "button[type=submit][name=loginButtonUniversal]")
     public WebElement login_submit_btn;
 
     @FindBy(how = How.CSS, using = "form[id=loginForm][name=loginForm]")
